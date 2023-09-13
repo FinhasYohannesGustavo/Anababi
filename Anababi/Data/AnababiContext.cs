b3decc52645df7df3f8cb407d747b84e0d17926f
Fellasfaw <fellasfaw@outlook.com>
2023-09-13 21:49:23 +0300

    Added AnababiContext.

---
+++ Anababi/Data/AnababiContext.cs
@@ -0,0 +1,26 @@
+﻿using Anababi.ModelClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anababi.Data
+{
+    public class AnababiContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Reference> References { get; set; } = null!;
+        public DbSet<PhysicalReference> PhysicalReferences { get; set; } = null!;
+        public DbSet<DigitalReference> DigitalReferences { get; set; } = null!;
+        public DbSet<Library> Libraries { get; set; } = null!;
+        public DbSet<Creator> Creators { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"server = .; 
+                database = AnababiLMS; 
+                integrated security = true; 
+                encrypt = false"
+            );
+        }
+
+    }
+}
