957600f5752b606ac9cf7c1e9570c73219028149
Fellasfaw <fellasfaw@outlook.com>
2023-09-14 20:50:10 +0300

    Removed GetFileType() from DigitalReference.

--- Anababi/ModelClasses/DigitalReference.cs
+++ Anababi/ModelClasses/DigitalReference.cs
@@ -14,12 +14,6 @@ namespace Anababi.ModelClasses
         {
             File = digitalReference.File;
         }
-        public string getFileType()
-        {
-            string fileType = "";
-            //Code for extracting the file type from the file.
-            return fileType;
-        }
 
     }
 }
