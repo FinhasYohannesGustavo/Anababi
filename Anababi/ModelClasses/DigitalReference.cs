caf38f4be4bf876cd5711c7a554096924f49cc3f
Fellasfaw <fellasfaw@outlook.com>
2023-09-14 14:28:56 +0300

    Added parameterized constructors for the reference classes.

--- Anababi/ModelClasses/DigitalReference.cs
+++ Anababi/ModelClasses/DigitalReference.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.Data.SqlTypes;
+using System.Transactions;
 
 namespace Anababi.ModelClasses
 {
     public class DigitalReference : Reference
     {
         public byte[] File { get; set; } = null!;
+
+        public DigitalReference() { }
+
+        public DigitalReference(DigitalReference digitalReference)
+            :base(digitalReference)
+        {
+            File = digitalReference.File;
+        }
         public string getFileType()
         {
             string fileType = "";
